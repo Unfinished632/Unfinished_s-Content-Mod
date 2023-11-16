5763e734d0357f0e4e3eb5ef88b908f6b0647f28
Unfinished_ <emuhammedemir@gmail.com>
2023-11-16 22:17:31 +0300

    Removed LICENSE file that has the wrong license

--- src/main/java/unfinished/unfinisheds_content_mod/item/ModItems.java
+++ src/main/java/unfinished/unfinisheds_content_mod/item/ModItems.java
@@ -1,9 +1,18 @@
 package unfinished.unfinisheds_content_mod.item;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import unfinished.unfinisheds_content_mod.Unfinished_sContentMod;
 
 public class ModItems
 {
+    private static Item RegisterItem(String name, Item item)
+    {
+        return Registry.register(Registries.ITEM, new Identifier(Unfinished_sContentMod.MOD_ID, name), item);
+    }
+
     public static void RegisterModItems()
     {
         Unfinished_sContentMod.LOGGER.info("Registering Mod Items for " + Unfinished_sContentMod.MOD_ID);
